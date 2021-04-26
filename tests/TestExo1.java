f3be6d925c967b21342bf825fe97fe1e8f308a16
HIcham SADDEK <h.saddek@serrahub.com>
2021-04-26 12:32:38 +0200

    Adding tests

---
+++ tests/TestExo1.java
@@ -0,0 +1,50 @@
+package test;
+
+import main.Main;
+import main.exo1.Model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class TestsExo1 {
+
+    private Model model;
+
+    @BeforeEach
+    public void freshModel(){
+
+        this.model = new Model(Main.buildTasks());
+    }
+
+    @Test
+    void sortModelTest() {
+
+        this.model.sortDescending();
+        //Assertions.assertEquals(,);
+    }
+
+    @Test
+    void penaltyNotNegativeTest(){
+
+
+    }
+
+    @Test
+    void buildCorrectlyTest(){
+
+
+    }
+
+    @Test
+    void valueMinLimitTest(){
+
+
+    }
+
+    @Test
+    void pathMinLimitTest(){
+
+
+    }
+}
+
